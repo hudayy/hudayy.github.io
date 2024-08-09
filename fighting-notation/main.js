a67408ca583b328166a9a72650bdec4f7842f644
Huday <jjdf2003@gmail.com>
2024-08-09 13:41:51 -0500

    added fighting notation conversion page

---
+++ fighting-notation/main.js
@@ -0,0 +1,22 @@
+const numberToArrow = {
+    '1': '↙️',
+    '2': '⬇️',
+    '3': '↘️',
+    '4': '⬅️',
+    '6': '➡️',
+    '7': '↖️',
+    '8': '⬆️',
+    '9': '↗️'
+};
+
+document.getElementById('numberInput').addEventListener('input', function() {
+    const input = this.value;
+    let output = '';
+
+    for (let char of input) {
+        if (char === '5') continue;
+        output += numberToArrow[char] || char;
+    }
+
+    document.getElementById('output').innerText = output;
+});
